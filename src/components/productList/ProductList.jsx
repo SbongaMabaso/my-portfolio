125a59a1626d7dd9d9bcf094e2a3aab323635d79
SbongaMabaso <63614632+SbongaMabaso@users.noreply.github.com>
2022-06-16 14:06:53 +0200

    Add files via upload

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -8,14 +8,12 @@ const ProductList = () => {
       <div className="pl-texts">
         <h1 className="pl-title">Completed Tasks</h1>
         <p className="pl-desc">
-          The following project has been completed with JavaScript, HTML and CSS.
-          I created this portfolio with Reat.js framework. The <a href="https://semo-solution.vercel.app/">Semo-Solution</a>  was created with Next.js,
-          whereas <a href="https://researchboard.netlify.app/">Research-board</a> and <a href="https://sneaker-sa.netlify.app/">Sneaker-SA</a> was created with poor JavaScript.
+          Check out my recent completed projects. feel free to contact to for any question or more information on the project.
         </p>
       </div>
       <div className="pl-list">
         {products.map((item) => (
-          <Product key={item.id} img={item.img} link={item.link} />
+          <Product key={item.id} img={item.img} /> //link={item.link}
         ))}
       </div>
     </div>
